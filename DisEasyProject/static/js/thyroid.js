33df90972b2f09316436325d84343515ecf4b151
Joel Parks <jpp63@drexel.edu>
2017-05-08 18:03:37 -0400

    CSS/HTML & Javascript for Thyroid
    Added the CSS/HTML in the thyroid form file, as well as the thyroid.js
    file for submitting the form.

---
+++ DisEasyProject/static/js/thyroid.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  //Updates the value of the dropdown menus
+  $('.dropdown-menu > li').click(function() {
+    var $toggle = $(this).parent().siblings('.dropdown-toggle');
+    $toggle.html("<i class=\"icon icon-envelope icon-white\"></i> " + $(this).text() + "  <span class=\"caret\"></span>")
+  });
+
+  //Submit button click
+  $('#submitButton').click(function() {
+	var isValid = true;  
+	  
+	var genderVal = $('#genderDropdown').text();
+    var ageVal = $('#ageInput').val();
+	var labVal = $('#labDropdown').text();
+    var testOneVal = $('#testOneInput').val();
+    var testTwoVal = $('#testTwoInput').val();
+	var testThreeVal = $('#testThreeInput').val();
+	
+	var ageNum = parseInt(ageVal);
+	var testOneNum = parseInt(testOneVal);
+	var testTwoNum = parseInt(testTwoVal);
+	var testThreeNum = parseInt(testThreeVal);
+	
+	var jString = {};
+	jString["gender"] = genderVal
+	jString["age"] = ageVal;
+	jString["lab"] = labVal;
+	jString["testOne"] = testOneVal;
+	jString["testTwo"] = testTwoVal;
+	jString["testThree"] = testThreeVal;
+	
+	//alert(isValid);
+	
+	if(jString.gender !== " Male  " && jString.gender !== " Female  "){
+		$('#genderWarning').show();
+		isValid = false;
+	}
+	if(jString.gender == " Male  " || jString.gender == " Female  "){
+		$('#genderWarning').hide();
+	}
+	if(jString.lab !== " Quest Diagnostics  " && jString.lab !== " LabCorp  "){
+		$('#labWarning').show();
+		isValid = false;
+	}
+	if(jString.lab == " Quest Diagnostics  " || jString.lab == " LabCorp  "){
+		$('#labWarning').hide();
+	}
+	if(isNaN(ageNum)) {
+		$('#ageWarning').show();
+		isValid = false;
+	}else{
+		$('#ageWarning').hide();
+	}
+	if(isNaN(testOneNum)) {
+		$('#testOneWarning').show();
+		isValid = false;
+	}else{
+		$('#testOneWarning').hide();
+	}
+	if(isNaN(testTwoNum)) {
+		$('#testTwoWarning').show();
+		isValid = false;
+	}else{
+		$('#testTwoWarning').hide();
+	}
+	if(isNaN(testThreeNum)) {
+		$('#testThreeWarning').show();
+		isValid = false;
+	}else{
+		$('#testThreeWarning').hide();
+	}
+	
+	if(isValid){
+    console.log(jString);
+		$.post("/submit", jString,
+		function(data, status){
+			window.location = data;
+		});
+	}
+  });
+});
